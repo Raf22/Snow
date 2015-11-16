7a31af2d563778042ca18497ecffffb8d268ed49
xelth <snowburstjustice@gmail.com>
2015-11-16 03:18:33 -0500

    todo list, model graphic functions

--- Snow/src/Graphics/GraphicsFunctions.h
+++ Snow/src/Graphics/GraphicsFunctions.h
@@ -9,13 +9,18 @@ Mesh* CreateMesh(const char* path, const char* name);
 Mesh* GetMesh(const char* meshName, std::vector<Mesh>&meshList);
 void DestroyMesh(const char* meshName, std::vector<Mesh>&meshList);
 
-bool LoadTex();
-Tex* CreateTex();
-Tex* GetTex(const char* texName);
-void DestroyTex(const char* texName);
+bool LoadTex(const char* path, Tex& tex);
+Tex* CreateTex(const char* path, const char* name);
+Tex* GetTex(const char* texName, std::vector<Tex>&texList);
+void DestroyTex(const char* texName, std::vector<Tex>&texList);
+
+Model* CreateModel(Mesh *mesh, Tex *tex, const char* name);
+Model* GetModel(const char* modelName, std::vector<Model>&modelList);
+void DestroyModel(const char* modelName, std::vector<Model>&modelList);
 
 void DrawMesh();
 void DrawTexturedMesh();
+void DrawModel();
 void DrawPoint();
 void DrawVector();
 void DrawAABB();
